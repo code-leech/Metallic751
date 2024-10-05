462f1172a338f1ec52be7649e10b2b7f750bf2f6
CodeLeech <109420988+code-leech@users.noreply.github.com>
2024-10-05 13:06:51 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 const obfuscate = true;
-const rammerhead = true;
+const rammerhead = false;
 const github = "https://github.com/cognetwork-dev/Metallic";
 const discord = "https://discord.com/invite/yk33HZSZkU";
 const privacyPolicyUpdated = 1707797140659;
